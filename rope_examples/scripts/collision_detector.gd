01235fd5570ae596722e175d64f83893b6867de3
Idan Bar <idan2bar@gmail.com>
2024-08-17 14:40:48 +0300

    Added the art

--- rope_examples/scripts/collision_detector.gd
+++ rope_examples/scripts/collision_detector.gd
@@ -4,14 +4,14 @@ extends Area2D
 @onready var indicator: Label = $indicator
 
 func _ready() -> void:
-    body_entered.connect(_body_entered)
-    body_exited.connect(_body_exited)
-    indicator.visible = false
+	body_entered.connect(_body_entered)
+	body_exited.connect(_body_exited)
+	indicator.visible = false
 
 
 func _body_entered(_body: Node2D) -> void:
-    indicator.visible = true
+	indicator.visible = true
 
 
 func _body_exited(_body: Node2D) -> void:
-    indicator.visible = false
+	indicator.visible = false
