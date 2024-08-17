4af99072e706f12feaf3d9f5cde8e5e05f29cbcb
amit zuarets <amitzzz2002@gmail.com>
2024-08-17 14:20:28 +0300

    finished code and fixed bugs

--- Scripts/server.gd
+++ Scripts/server.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 
 var connected_server: Server
 <<<<<<< HEAD
+<<<<<<< HEAD
 @onready var connection_point: ConnectionPoint = $ConnectionPoint
 var is_blinking = false
 var blink_color: Color
@@ -19,12 +20,25 @@ func set_blinking_color(color: Color):
 	blink_color = color
 =======
 @onready var connection_point = $ConnectionPoint
+=======
+@onready var connection_point: ConnectionPoint = $ConnectionPoint
+>>>>>>> 2915dc2 (finished code and fixed bugs)
 var is_blinking = false
 var blink_color: Color
 
+func _process(delta):
+	if is_blinking:
+		self.connection_point.blink(blink_color)
+
 func set_connected_server(server):
 	self.connected_server = server
+<<<<<<< HEAD
 >>>>>>> 2ffcf51 (created server class, started work on server choosing algorithm)
+=======
+	
+func set_blinking_color(color: Color):
+	blink_color = color
+>>>>>>> 2915dc2 (finished code and fixed bugs)
 
 func set_is_blinking(value):
 	self.is_blinking = value
