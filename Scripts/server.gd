1794a17baa1ecc3fdad69a96e7098c66d515da1a
amit zuarets <amitzzz2002@gmail.com>
2024-08-17 12:51:46 +0300

    created server class, started work on server choosing algorithm

--- Scripts/server.gd
+++ Scripts/server.gd
@@ -3,6 +3,7 @@ class_name Server
 extends StaticBody2D
 
 var connected_server: Server
+<<<<<<< HEAD
 @onready var connection_point: ConnectionPoint = $ConnectionPoint
 var is_blinking = false
 var blink_color: Color
@@ -16,6 +17,14 @@ func set_connected_server(server):
 	
 func set_blinking_color(color: Color):
 	blink_color = color
+=======
+@onready var connection_point = $ConnectionPoint
+var is_blinking = false
+var blink_color: Color
+
+func set_connected_server(server):
+	self.connected_server = server
+>>>>>>> 2ffcf51 (created server class, started work on server choosing algorithm)
 
 func set_is_blinking(value):
 	self.is_blinking = value
