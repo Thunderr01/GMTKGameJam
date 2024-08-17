b80c00e9c13b5070ab65bfc4078f7c95eb1b861d
amit zuarets <amitzzz2002@gmail.com>
2024-08-17 12:51:46 +0300

    created server class, started work on server choosing algorithm

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,14 +1,4 @@
 extends Node
 
-var servers = []
-var used_colors = []
-var rng = RandomNumberGenerator.new()
-
 func _ready():
-	pass
-
-func link_two_servers():
-	pass
-
-func get_all_unblinking_servers():
 	pass
