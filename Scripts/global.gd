715b200bed0a1d670c2690a6c54024cebfa09cc3
amit zuarets <amitzzz2002@gmail.com>
2024-08-17 11:56:46 +0300

    starting work on randomizing servers blinking

---
+++ Scripts/global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var servers = []
+var used_colors = []
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	pass
+
+func link_two_servers():
+	pass
+
+func get_all_unblinking_servers():
+	pass
