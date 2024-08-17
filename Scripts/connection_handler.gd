6baf70e6df5995c2574b6c43d3a0c38c7a6c2227
amit zuarets <amitzzz2002@gmail.com>
2024-08-17 14:27:39 +0300

    .

--- Scripts/connection_handler.gd
+++ Scripts/connection_handler.gd
@@ -46,9 +46,8 @@ func _process(delta: float) -> void:
 	
 func _on_area_detect_connection_body_entered(body: Node2D) -> void:
 	_current_connection_point = body
-	body.detect()
-
+	print(body.get_class())
+	
 func _on_area_detect_connection_body_exited(body: Node2D) -> void:
 	if _current_connection_point == body:
 		_current_connection_point = null
-		body.undetect()
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,9 +1,71 @@
 extends Node2D
 
+@onready var servers = $Servers
+@onready var timer = $Timer
+var available_colors = [Color.RED, Color.PURPLE, Color.ORANGE, Color.DEEP_PINK]
+var used_colors = []
+var rng = RandomNumberGenerator.new()
+var time
+var time_of_last_blinking = 0.0
+var time_per_blink = 2.0
 
 func _ready() -> void:
-	pass
-
+	timer.wait_time = 9999
+	timer.start()
 
 func _process(delta: float) -> void:
-	pass
+	time = timer.wait_time - timer.time_left
+	if time - time_of_last_blinking >= time_per_blink:
+		blink_two_servers()
+		time_of_last_blinking += 10.0		
+	
+func blink_two_servers():
+	var unblinking_servers = get_all_unblinking_servers()
+	
+	if unblinking_servers.size() < 2:
+		return
+		
+	var random_servers = get_two_random_servers(unblinking_servers)
+	var server1: Server = random_servers[0]
+	var server2: Server = random_servers[1]
+	
+	server1.set_connected_server(server2)
+	server2.set_connected_server(server1)
+	
+	var server1_color =  get_random_unused_color()
+	server1.set_blinking_color(server1_color)
+	used_colors.append(server1_color)
+	var server2_color =  get_random_unused_color()
+	server2.set_blinking_color(server1_color)
+	used_colors.append(server2_color)
+	
+	server1.set_is_blinking(true)
+	server2.set_is_blinking(true)
+
+func get_all_unblinking_servers():
+	var unblinking_servers = []
+	
+	for server: Server in servers.get_children():
+		if not server.is_blinking:
+			unblinking_servers.append(server)
+			
+	return unblinking_servers
+	
+func get_two_random_servers(servers: Array):
+	var server1_index = 0
+	var server2_index = 0
+	
+	while server1_index == server2_index:
+		server1_index = rng.randi_range(0, servers.size() - 1)
+		server2_index = rng.randi_range(0, servers.size() - 1)	
+		
+	return [servers[server1_index], servers[server2_index]]
+	
+	
+func get_random_unused_color():
+	var chosen_color: Color
+	
+	while used_colors.has(chosen_color):
+		chosen_color = available_colors[rng.randi_range(0, available_colors.size() - 1)]
+		
+	return chosen_color
